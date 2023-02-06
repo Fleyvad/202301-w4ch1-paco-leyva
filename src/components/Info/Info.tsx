00f881c0790b8135182a249f69277a37c892d0b4
Paco Leyva <118618592+Fleyvad@users.noreply.github.com>
2023-02-06 20:43:43 +0100

    create Info components

---
+++ src/components/Info/Info.tsx
@@ -0,0 +1,12 @@
+import "./Info.css";
+
+const Info = () => {
+  return (
+    <header className="main-header">
+      <h1 className="main-title">The pointing gentlemen</h1>
+      <p className="info">0 gentlemen pointing at you</p>
+    </header>
+  );
+};
+
+export default Info;
